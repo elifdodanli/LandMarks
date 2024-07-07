e70b430c4f2af0a203543751c77638df598e6577
Elif <elfdodanli@gmail.com>
2024-07-07 14:47:07 +0300

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Elif Dodanlı on 7.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
